00edf960d9eed6c08cd1073e9a354defc64e274a
jrfibel <jason.feibelman@gmail.com>
2023-08-26 16:33:29 -0400

    finished slack funcs

---
+++ recap/slack.py
@@ -0,0 +1,39 @@
+from recap.slack import WebClient
+from slack.errors import SlackApiError
+from yaml import load, SafeLoader
+from datetime import datetime, timedelta
+
+SLACK_KEY = "SlackKey"
+CHANNEL_KEY = "ChannelKey"
+
+def fetch_slack_messages(config: str):
+    with open(config) as cfg:
+        d = load(cfg, SafeLoader)
+        if SLACK_KEY not in d or CHANNEL_KEY not in d:
+            print("Error: 'SlackKey' not found in configs")
+            return False
+
+        client = WebClient(token=d[SLACK_KEY])
+        last_time = datetime.now() - timedelta(hours=3, minutes=0)
+
+        return client.conversations_history(oldest=last_time.timestamp(), channel=d[CHANNEL_KEY])
+
+def post_slack_messages(config: str, msg: str, channel: str="general"):
+  with open(config) as cfg:
+      d = load(cfg, SafeLoader)
+      if SLACK_KEY not in d:
+          print("Error: 'SlackKey' not found in configs")
+          return False
+
+  client = WebClient(token=d[SLACK_KEY])
+
+  try:
+      response = client.chat_postMessage(
+      channel=channel,
+      text=msg
+      )
+  except SlackApiError as e:
+      assert e.response["error"]
+      return False
+  
+  return True
