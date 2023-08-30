ac100f7bcc47e7e496ddeac924fadad7f0392ece
jrfibel <jason.feibelman@gmail.com>
2023-08-30 18:25:09 -0400

    final touches

--- recap/main.py
+++ recap/main.py
@@ -1,20 +1,31 @@
 from typing import Union
-from fastapi import FastAPI, APIRouter
+# from fastapi import FastAPI, APIRouter
+# import uvicorn
+from slack import fetch_slack_messages, post_slack_messages
+from summarize_openai import openaiCall
+import os
+import yaml
 
-import uvicorn
-
-app = FastAPI()
-api_router = APIRouter()
-app.include_router(api_router)
+# app = FastAPI()
+# api_router = APIRouter()
+# app.include_router(api_router)
 
 
 
-@app.get("/")
-async def root():
-    return {"msg": "Hello world!"}
+# @app.get("/")
+# async def root():
+#     return {"msg": "Hello world!"}
 
-@app.get("/items/{item_id}")
-def update_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# def update_item(item_id: int):
+#     return {"item_id": item_id}
+
+def connecting(): 
+    convo_history = fetch_slack_messages("%s/config/slack.yaml"% os.getcwd()) #putting in root directory
+    getAIRepsonse = openaiCall(convo_history)
+    print(getAIRepsonse)
+
+connecting()
+
 
 
--- recap/slack.py
+++ recap/slack.py
@@ -1,9 +1,13 @@
 from slack_sdk import WebClient
+import slack_sdk
+from slack_sdk.errors import SlackApiError
 from yaml import load, SafeLoader
 from datetime import datetime, timedelta
+import os
 
 SLACK_KEY = "SlackKey"
 CHANNEL_KEY = "ChannelKey"
+
 
 def fetch_slack_messages(config: str):
     with open(config) as cfg:
@@ -18,21 +22,23 @@ def fetch_slack_messages(config: str):
         return client.conversations_history(oldest=last_time.timestamp(), channel=d[CHANNEL_KEY])
 
 def post_slack_messages(config: str, msg: str, channel: str="general"):
-  with open(config) as cfg:
-      d = load(cfg, SafeLoader)
-      if SLACK_KEY not in d:
-          print("Error: 'SlackKey' not found in configs")
-          return False
+    with open(config) as cfg:
+        d = load(cfg, SafeLoader)
+        if SLACK_KEY not in d:
+            print("Error: 'SlackKey' not found in configs")
+            return False
+    
+    client = WebClient(token=d[SLACK_KEY])
 
-  client = WebClient(token=d[SLACK_KEY])
+    try:
+        response = client.chat_postMessage(
+        channel=channel,
+        text=msg
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
+        return False
 
-  try:
-      response = client.chat_postMessage(
-      channel=channel,
-      text=msg
-      )
-  except SlackApiError as e:
-      assert e.response["error"]
-      return False
-  
-  return True
+    return True
+
+# fetch_slack_messages("%s/config/slack.yaml"% os.getcwd())
